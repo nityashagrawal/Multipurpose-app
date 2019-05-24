8c8d771ff4310f941fdfce435d2dbaa147f90927
Nityash <nityashagrawal0@gmail.com>
2019-05-25 01:08:34 +0530

    Make two different buttons for the IP and the location

--- App.js
+++ App.js
@@ -1,57 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button, TouchableOpacity, ImageBackground} from 'react-native';
 export default class App extends Component{
 
-  constructor(props) {console.log(1)
-    super(props);
-    this.state = { showIP: false,
-      blah: {},
-      moreblah: {}, };
-  //     fetch('https://api.ipify.org?format=json').then(response => response.json()).then(result => {
-  //     const abc='https://ipvigilante.com/json/' + result.ip
-  //     fetch(abc).then(response => response.json()).then(result1 => this.setState({ blah: result, moreblah: result1}))
-  // }) 
-    }
+  state = { 
+      showIP1: false,
+      showIP2: false,
+      currentIp: '',
+      ipDetails: {}, 
+  };
 
-  IpPressed = () => {
-    this.setState({showIP:true})
-  }
-  componentDidUpdate(){
+find = () => {
+    this.setState({showIP1:true})
     fetch('https://api.ipify.org?format=json').then(response => response.json()).then(result => {
-      const abc='https://ipvigilante.com/json/' + result.ip
-      fetch(abc).then(response => response.json()).then(result1 => this.setState({ blah: result, moreblah: result1}))
-  }) 
+      this.setState({
+        currentIp: result.ip
+      })
+    })
+  }
+  findMore = () => {
+    this.setState({showIP2:true})
+    fetch('https://ipvigilante.com/json/'+ this.state.currentIp).then(response => response.json()).then(result => {
+      this.setState({
+        ipDetails: result.data
+      })
+    })
   }
   
   render() {
+     
       return (
       <View style={styles.container}>
         <ImageBackground
-        style={{
-          flex: 1,
-          resizeMode: 'center',
-        }}
-        source={{ uri: 'https://static.tvtropes.org/pmwiki/pub/images/voldemort.jpg' }}>
-        <Text style={styles.welcome}>Multipurpose App</Text>
-        <View style={styles.row}><TouchableOpacity style ={styles.button} onPress={this.IpPressed}>
-          <Text>Find My IP!!</Text>
-        </TouchableOpacity></View>
-        {this.state.moreblah.data&&<View >
-        {this.state.showIP&&<Text  style={styles.ip}>Your IP Address is: {this.state.blah.ip}</Text>}
-        {this.state.showIP&&<Text  style={styles.ip}>Your Continent is: {this.state.moreblah.data.continent_name}</Text>}
-        {this.state.showIP&&<Text  style={styles.ip}>Your Country is: {this.state.moreblah.data.country_name}</Text>}
-        {this.state.showIP&&<Text  style={styles.ip}>Your City is: {this.state.moreblah.data.city_name}</Text>}
-        {this.state.showIP&&<Text style={styles.ip}>Your Latitude  is: {this.state.moreblah.data.latitude}</Text>}
-       {this.state.showIP&&<Text  style={styles.ip}>Your Longitude is: }{this.state.moreblah.data.longitude}</Text>}
-        </View>}
+          style={{
+            flex: 1,
+            resizeMode: 'center',
+          }}
+          source={{ uri: 'https://static.tvtropes.org/pmwiki/pub/images/voldemort.jpg' }}>
+          <Text style={styles.welcome}>Multipurpose App</Text>
+          <View style={styles.row}>
+            <TouchableOpacity style ={styles.button} onPress={this.find}>
+              <Text>Find My IP!!</Text>
+            </TouchableOpacity></View>
+            {this.state.showIP1&&<Text  style={styles.ip}>Your IP Address is: {this.state.currentIp}</Text>}
+            <View style={styles.row}>
+            <TouchableOpacity style ={styles.button} onPress={this.findMore}>
+              <Text>Find My Location!!</Text>
+            </TouchableOpacity></View>
+            {this.state.showIP2&&(this.state.currentIp!='')&&<View>
+            <Text  style={styles.ip}>Your Continent is: {this.state.ipDetails.continent_name}</Text>}
+            <Text  style={styles.ip}>Your Country is: {this.state.ipDetails.country_name}</Text>}
+            <Text  style={styles.ip}>Your City is: {this.state.ipDetails.city_name}</Text>}
+            <Text style={styles.ip}>Your Latitude  is: {this.state.ipDetails.latitude}</Text>}
+            <Text  style={styles.ip}>Your Longitude is: }{this.state.ipDetails.longitude}</Text>}
+            </View>}
         </ImageBackground>
         </View>
     );
